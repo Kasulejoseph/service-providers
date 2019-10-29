2146cb635bff3c02467ea82bb5ec5e90798ec949
kasule joseph <joseph.kasule@andela.com>
2019-10-29 18:01:17 +0200

    fix openshift deployment

--- src/connect.js
+++ src/connect.js
@@ -7,6 +7,8 @@ let mongodb_connection_string = process.env.LOCAL_URL
 if(process.env.NODE_ENV == 'production'){
     mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL;
   }
+  console.log(mongodb_connection_string);
+  
 mongoose.connect(mongodb_connection_string, {
     useNewUrlParser: true,
     useCreateIndex: true,
