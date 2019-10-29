030d6c129468a141aef5d11804e608e6549770aa
kasule joseph <joseph.kasule@andela.com>
2019-10-29 06:22:26 +0200

    Fix openshift

--- src/connect.js
+++ src/connect.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 let mongodb_connection_string = process.env.LOCAL_URL
-if(process.env.ENV == 'production'){
+if(process.env.NODE_ENV == 'production'){
     mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL;
   }
 mongoose.connect(mongodb_connection_string, {
