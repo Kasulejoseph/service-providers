90dc21b8fb5c0d46f606e5718fde0c453fd80cf9
kasule joseph <joseph.kasule@andela.com>
2019-11-12 12:24:45 +0200

    heroku deploy

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use(express.json())
 app.use(cors())
 const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080
 const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '0.0.0.0'
+console.log(port, server_ip_address);
+
 app.use(router)
 app.listen(port, server_ip_address)
 
